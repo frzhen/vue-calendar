0848003bf223699ce2c5b8370a1976caa1ac9adc
Fred Zhen <fred.zhen@1dataops.com>
2021-07-24 19:15:58 +0800

    Rebuild the vue template with resolved sass-loader issue(10.0.4), dart-sass, sass modules

--- README.md
+++ README.md
@@ -1,24 +1,53 @@
 # calendar
 
 ## Project setup
-```
+
+```script
 npm install
 ```
 
 ### Compiles and hot-reloads for development
-```
+
+```script
 npm run serve
 ```
 
 ### Compiles and minifies for production
-```
+
+```script
 npm run build
 ```
 
 ### Lints and fixes files
-```
+
+```script
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Vue Component Structure
+
+```mermaid
+graph TD;
+    id1(App)-->id2([CalendarEntry])
+    id1(App)-->id3[[CalendarWeek]]
+    id3[[CalendarWeek]]-->id4[[CalendarDay]]
+    id4[[CalendarDay]]-->id5([CalendarEvent])
+```
+
+#### gantt chart example
+
+```mermaid
+gantt
+    title Gantt Diagram
+    dateFormat YYYY-MM-DD
+    section Frontend-1
+    CalendarEvent         :a1, 2021-07-23, 2021-07-24
+    CalendarEntry         :active, after a1, 2d
+    section Frontend-2
+    CalendarDay           :crit, a2, 2021-07-22, 2021-07-24
+    CalendarWeek          :after a2, 2021-07-27
+```
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <CalendarWeek />
+    <CalendarEntry />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CalendarWeek from './components/CalendarWeek.vue'
+import CalendarEntry from './components/CalendarEntry.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CalendarWeek,
+    CalendarEntry
   }
 }
+
 </script>
 
-<style>
+<style land="scss">
+  html, body {
+    height: 100%;
+    background: #eee;
+  }
+</style>
+
+<style lang="scss" scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    /* SCSS for app */
+    background: #6e6e6e;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    -webkit-align-items: center;
+    justify-content: center;
+    -webkit-justify-content: center;
+  }
 </style>
